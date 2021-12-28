36f6bc0a971fa80bfe7cb6d39dcb3e5e893a2c60
Adriano Nascimento de Santana <adriano.santana@fastsolucoes.com.br>
2021-12-28 10:52:08 -0300

    Add score keeper

--- lib/main.dart
+++ lib/main.dart
@@ -25,21 +25,30 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Widget> scoreKeeper = [];
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.'
+  ];
+
+  int questionIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: <Widget>[
-        const Expanded(
+        Expanded(
           flex: 5,
           child: Padding(
-            padding: EdgeInsets.all(10.0),
+            padding: const EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[questionIndex],
                 textAlign: TextAlign.center,
-                style: TextStyle(
+                style: const TextStyle(
                   fontSize: 25.0,
                   color: Colors.white,
                 ),
@@ -61,7 +70,13 @@ class _QuizPageState extends State<QuizPage> {
                   fontSize: 20.0,
                 ),
               ),
-              onPressed: () => print("True"),
+              onPressed: () => setState(() {
+                questionIndex++;
+                scoreKeeper.add(const Icon(
+                  Icons.check,
+                  color: Colors.green,
+                ));
+              }),
             ),
           ),
         ),
@@ -79,11 +94,19 @@ class _QuizPageState extends State<QuizPage> {
                   fontSize: 20.0,
                 ),
               ),
-              onPressed: () => print("True"),
+              onPressed: () => setState(() {
+                questionIndex++;
+                scoreKeeper.add(const Icon(
+                  Icons.close,
+                  color: Colors.red,
+                ));
+              }),
             ),
           ),
-        )
-        // : Add a Row here as your score keeper
+        ),
+        Row(
+          children: scoreKeeper,
+        ),
       ],
     );
   }
